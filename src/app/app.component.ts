c941a53e752b284f378b5981706e500aa4664012
sajalverma17 <sajalv128@gmail.com>
2023-03-12 04:27:26 +0100

    Add commit changes accidentally left out from previous commit made from inside keywords folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'expense-extractor';
+  title = 'expense extractor';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KeywordsComponent } from './keywords/keywords.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeywordsComponent
   ],
   imports: [
     BrowserModule,
