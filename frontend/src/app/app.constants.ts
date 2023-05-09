639dd71892a7af9b2767f7bb26c141e6e8ccaa29
sajalverma17 <sajalv128@gmail.com>
2023-05-09 19:00:38 +0200

    chore: update internal and external port numbers

--- frontend/src/app/app.constants.ts
+++ frontend/src/app/app.constants.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = 'expense extractor'
-export const API_SERVER_URI = "http://localhost:8000"
+export const API_SERVER_URI = "http://localhost:8002"
 export const KEYWORDS = ["kiwi", "ruter", "BKH"]
